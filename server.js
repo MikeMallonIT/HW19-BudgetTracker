7300806921510ab2e0e3fb3bc944e73cfdafd5a2
Mike Mallon <mallon127@gmail.com>
2022-01-23 21:47:54 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
@@ -13,7 +12,8 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
+
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/Budget-Tracker',
